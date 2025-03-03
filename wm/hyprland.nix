41fcfa387433ac786711153d08b743e3b032e587
aki-repo <148870527+aki-repo@users.noreply.github.com>
2025-03-03 09:13:11 -0500

    Update hyprland.nix

--- wm/hyprland.nix
+++ wm/hyprland.nix
@@ -1,1 +1,11 @@
 # hyprland configuration
+
+{inputs, pkgs, ...}: {
+  programs.hyprland = {
+    enable = true;
+    # set the flake package
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    # make sure to also set the portal package, so that they are in sync
+    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+  };
+}
