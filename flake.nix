731958db29abe3e4e5cb7208f7ae478504b055a7
aki-repo <148870527+aki-repo@users.noreply.github.com>
2025-03-03 09:15:18 -0500

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -9,6 +9,7 @@
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     stylix.url = "github:danth/stylix";
+    inputs.hyprland.url = "github:hyprwm/Hyprland";
     
   };
 
