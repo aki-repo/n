3d7ba58afd844210fc9bde09672c23b3b50e51a9
aki-repo <148870527+aki-repo@users.noreply.github.com>
2025-03-03 10:08:52 -0500

    cachix for hypr

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -168,8 +168,12 @@
   # Install firefox.
     programs.firefox.enable = true;
 
-  # stylix
-  # stylix.enable = true;
+  
+  nix.settings = {
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
+
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
