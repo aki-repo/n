dafc73ec7424291663499c5f545b4181a700bb40
aki-repo <148870527+aki-repo@users.noreply.github.com>
2025-03-03 12:29:31 -0500

    Create niri.nix

---
+++ wm/niri.nix
@@ -0,0 +1,16 @@
+{
+  niri,
+  ...
+}: {
+
+programs.niri.enable = true;
+      
+      environment.variables.NIXOS_OZONE_WL = "1";
+      environment.systemPackages = with pkgs; [
+        wl-clipboard
+        wayland-utils
+        xwayland-satellite
+        swaybg
+        waypaper
+      ];
+    }
