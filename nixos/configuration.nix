d8ffb85a54fbc909c97ab66b4e50bf7a267e38d3
aki-repo <148870527+aki-repo@users.noreply.github.com>
2025-03-03 07:57:16 -0500

    fix path to wm

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -16,7 +16,7 @@
 
     # You can also split up your configuration and import pieces of it here:
     # ./users.nix
-	../gnome.nix
+    ../wm/gnome.nix
     # Import your generated (nixos-generate-config) hardware configuration
     ./hardware-configuration.nix
   ];
