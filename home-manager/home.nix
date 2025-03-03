b2605f4ec2d7e5639a99af448755f17a6edee9f8
aki-repo <148870527+aki-repo@users.noreply.github.com>
2025-03-03 09:22:32 -0500

    Update home.nix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -52,6 +52,36 @@
   programs.git.enable = true;
   programs.micro.enable = true;
   programs.vscode.enable = true;
+
+  # Appearance
+
+  home.pointerCursor = {
+    gtk.enable = true;
+    # x11.enable = true;
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Classic";
+    size = 16;
+  };
+
+  gtk = {
+    enable = true;
+
+    theme = {
+      package = pkgs.flat-remix-gtk;
+      name = "Flat-Remix-GTK-Grey-Darkest";
+    };
+
+    iconTheme = {
+      package = pkgs.gnome.adwaita-icon-theme;
+      name = "Adwaita";
+    };
+
+    font = {
+      name = "Sans";
+      size = 11;
+    };
+  };
+
   
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
