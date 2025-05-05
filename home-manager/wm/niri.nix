b45e908679361aadea0b17bf6b0df780ca9dcf68
aki-repo <148870527+aki-repo@users.noreply.github.com>
2025-05-05 12:01:23 -0400

    Update niri.nix

--- home-manager/wm/niri.nix
+++ home-manager/wm/niri.nix
@@ -3,8 +3,7 @@
   ...
 }: {
 
-programs.niri.enable = true;
-      
+programs.niri.enable = true;      
       environment.variables.NIXOS_OZONE_WL = "1";
       environment.systemPackages = with pkgs; [
         wl-clipboard
