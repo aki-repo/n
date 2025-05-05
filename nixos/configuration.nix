466220600c9d11b37b18034dea147d2a88ca3d3a
aki-repo <148870527+aki-repo@users.noreply.github.com>
2025-05-05 12:09:36 -0400

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -16,14 +16,13 @@
 
     # You can also split up your configuration and import pieces of it here:
     # ./users.nix
-    ../gnome.nix
+    ../dwm.nix
     # Import your generated (nixos-generate-config) hardware configuration
     ./hardware-configuration.nix
   ];
 
   # kernel
   boot.kernelPackages = pkgs.linuxPackages_zen;
-
 
   # polkit
   security.polkit.enable = true;
@@ -75,10 +74,30 @@
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
+  services.xserver.displayManager.startx.enable = true;
 
-# Enable the GNOME Desktop Environment.
-#  services.xserver.displayManager.gdm.enable = true;
-#  services.xserver.desktopManager.gnome.enable = true;
+  # bigger tty fonts
+  console.font =
+    "${pkgs.terminus_font}/share/consolefonts/ter-u28n.psf.gz";
+  services.xserver.dpi = 180;
+  environment.variables = {
+    ## Used by GTK 3
+    # `GDK_SCALE` is limited to integer values
+    GDK_SCALE = "2";
+    # Inverse of GDK_SCALE
+    GDK_DPI_SCALE = "0.5";
+
+    # Used by Qt 5
+    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+
+    _JAVA_OPTIONS = "-Dsun.java2d.uiScale=2";
+  };
+  # Expose variables to graphical systemd user services
+  services.xserver.displayManager.importedVariables = [
+    "GDK_SCALE"
+    "GDK_DPI_SCALE"
+    "QT_AUTO_SCREEN_SCALE_FACTOR"
+  ];
 
   # Configure keymap in X11
   services.xserver.xkb = {
