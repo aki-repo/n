566c99a2b619d3c9fc87f30ef0c1e15fb2e8394b
Andreas Taenzer <andreast.work@gmail.com>
2026-01-17 17:20:39 -0500

    fix

---
+++ modules/noctalia.nix
@@ -0,0 +1,60 @@
+{ pkgs, inputs, ... }:
+{
+# configure options
+    programs.noctalia-shell = {
+      enable = true;
+      settings = {
+        # configure noctalia here
+        bar = {
+          density = "comfortable";
+          position = "left";
+          showCapsule = false;
+          widgets = {
+            left = [
+              {
+                id = "ControlCenter";
+                useDistroLogo = true;
+              }
+              {
+                id = "WiFi";
+              }
+              {
+                id = "Bluetooth";
+              }
+            ];
+            center = [
+              {
+                hideUnoccupied = false;
+                id = "Workspace";
+                labelMode = "none";
+              }
+            ];
+            right = [
+              {
+                alwaysShowPercentage = false;
+                id = "Battery";
+                warningThreshold = 30;
+              }
+              {
+                formatHorizontal = "HH:mm";
+                formatVertical = "HH mm";
+                id = "Clock";
+                useMonospacedFont = true;
+                usePrimaryColor = true;
+              }
+            ];
+          };
+        };
+        colorSchemes.predefinedScheme = "Monochrome";
+        general = {
+        };
+        location = {
+          monthBeforeDay = true;
+          name ="Washington, US";
+        };
+      };
+      # this may also be a string or a path to a JSON file.
+    };
+  };
+
+};
