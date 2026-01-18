bea6b0e91b7ca85397b15a45d2538a94923d0585
Andreas Taenzer <andreast.work@gmail.com>
2026-01-18 13:57:38 -0500

    g

---
+++ modules/gnome.nix
@@ -0,0 +1,43 @@
+{ config, pkgs, ... }:
+
+{
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
+
+  # To disable installing GNOME's suite of applications
+  # and only be left with GNOME shell.
+  services.gnome.core-apps.enable = false;
+  services.gnome.core-developer-tools.enable = false;
+  services.gnome.games.enable = false;
+  environment.gnome.excludePackages = with pkgs; [ gnome-tour gnome-user-docs ];
+
+  environment.systemPackages = with pkgs; [
+    gnomeExtensions.blur-my-shell
+    gnomeExtnsions.paperwm
+    gnomeExtensions.just-perfection
+    refine
+  ];
+
+  qt = {
+    enable = true;
+    platformTheme = "gnome";
+    style = "adwaita-dark";
+  };
+
+  programs.dconf.profiles.user.databases = [
+    {
+      settings = {
+        "org/gnome/mutter" = {
+          experimental-features = [
+            "scale-monitor-framebuffer" # Enables fractional scaling (125% 150% 175%)
+            "variable-refresh-rate" # Enables Variable Refresh Rate (VRR) on compatible displays
+            "xwayland-native-scaling" # Scales Xwayland applications to look crisp on HiDPI screens
+            "autoclose-xwayland" # automatically terminates Xwayland if all relevant X11 clients are gone
+          ];
+        };
+      };
+    }
+  ];
+
+
+}
