b86353f7341a7f66a5c044b5bb172a926ce7309c
Andreas Taenzer <andreast.work@gmail.com>
2026-01-20 15:52:10 -0500

    s

--- configuration.nix
+++ configuration.nix
@@ -83,7 +83,19 @@
 
   programs.niri.enable = true;
 
-  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.displayManager.lightdm = {
+  enable = true;
+
+  # Setting gtk as the greeter
+  greeters.gtk.enable = true;
+
+  # Example of having background as a particular color
+  background = "#ffa07a";
+
+  # Example of the default image background (must be an absolute path)
+  background = pkgs.nixos-artwork.wallpapers.simple-dark-gray-bottom.gnomeFilePath;
+
+};
 
   # Configure keymap in X11
   services.xserver.xkb = {
